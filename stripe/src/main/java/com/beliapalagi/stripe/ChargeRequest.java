90334c7f2dcc1c5493c7796126583984600c94b1
pratama rendra <chedoxtop@gmail.com>
2017-10-26 09:09:45 +0700

    Add files via upload

---
+++ stripe/src/main/java/com/beliapalagi/stripe/ChargeRequest.java
@@ -0,0 +1,15 @@
+package com.beliapalagi.stripe;
+
+import lombok.Data;
+
+@Data
+public class ChargeRequest {
+    public enum Currency {
+        EUR, USD;
+    }
+    private String description;
+    private int amount; // cents
+    private Currency currency;
+    private String stripeEmail;
+    private String stripeToken;
+}
